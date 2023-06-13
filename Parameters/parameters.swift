c5d16e87e9cfa0fb98bbe6b6dd9362c2e3231178
Calaruin <130497974+Calaruin@users.noreply.github.com>
2023-06-13 21:51:07 +0300

    Update parameters.swift

--- Parameters/parameters.swift
+++ Parameters/parameters.swift
@@ -1,18 +1,58 @@
-let totalGems = randomNumberOfGems
-var counterGems = 0
+let totalGems = RandomNumbersOfGems
+var gems = 0
 
-while !isBlocked && counterGems != totalGems {
-    moveForward()
-    if isBlocked && isBlockedLeft {
-        turnRight()
+let exp = Expert()
+
+let char = Character()
+
+world.place(exp, facing: north, atColumn: 0, row: 4)
+
+world.place(char, facing: south, atColumn: 4, row: 6)
+
+exp.turnLockUp()
+
+repeat {
+
+    for i in 1 ... 3 {
+
+        for i in 1 ... 6 {
+
+            if char.isOnGem {
+
+                char.collectGem()
+
+                gems += 1
+
+            }
+
+            char.jump()
+
+        }
+
+        switch i {
+        case 1:
+            char.turnRight()
+
+            char.moveForward()
+
+            char.turnRight()
+
+        case 2:
+            char.turnLeft()
+
+            char.moveForward()
+
+            char.turnLeft()
+
+        case 3:
+            char.turnLeft()
+
+            char.turnLeft()
+
+        default:
+            break 
+        }
+
     }
-    if isBlocked && isBlockedRight {
-        turnLeft()
-    }
-    
-    if isOnGem {
-        collectGem()
-        counterGems += 1
-    }
-    
-}
+
+} while gems != totalGems
